16d310ea367a2e8d38f11b2590fbf202cce5eb0f
Rachmad Hidayat <rachmad.eepis@gmail.com>
2020-02-07 19:32:07 +0700

    Create read90subject.py

---
+++ read90subject.py
@@ -0,0 +1,39 @@
+import cv2, os, glob, re, time, numpy
+import matplotlib.pyplot as plt
+
+start = time.time()
+def splitstring(word):
+    label = numpy.zeros(90)
+    personId = int(word[2:4])
+    label[personId-1] = 1
+    return label
+
+data = []
+label = []
+name = []
+# read several image
+for i in range(90):
+    print("read data : ",numpy.round(i/89*100,2),"%")
+    img_dir = "90subject-Cleaned/Subject" + str(i+1) # Enter Directory of all images 
+    data_path = os.path.join(img_dir,'*jpg')
+    files = glob.glob(data_path)
+
+    for f1 in files:
+        image = cv2.imread(f1)
+        base = os.path.basename(f1)
+        base = os.path.splitext(base)
+        degree = base[0][5:]
+        if(degree == '0' or degree == '+15' or degree == '-15' or degree == '+30' or degree == '-30' or degree == '+45' or degree == '-45'):
+            title = splitstring(base[0])
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            image = cv2.resize(image,(480,360))
+            # fit into list
+            data.append(image)
+            label.append(title)
+            name.append(base[0])
+
+data = numpy.array(data)
+label = numpy.array(label)
+name = numpy.array(name)
+end = time.time()
+print("read data complete " , round(end-start,2) , "s, total : ", len(data))
